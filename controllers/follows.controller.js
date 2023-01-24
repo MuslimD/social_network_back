63f1c7621ffb02edf3d1300b7ea35d2c8a8d2a70
muslim <mhas2506@gmail.com>
2023-01-24 14:49:26 +0300

    nnnn

--- controllers/follows.controller.js
+++ controllers/follows.controller.js
@@ -2,7 +2,10 @@ const Follows = require("../model/follows.model");
 module.exports.FollowsController = {
   getfollows: async (req, res) => {
     try {
-      const follows = await Follows.find({userid: req.params.userid});
+      const follows = await Follows.find({
+        userid: req.params.userid,
+      }).populate("follows");
+
       return res.json(follows);
     } catch (error) {
       return res.json(error.message);
